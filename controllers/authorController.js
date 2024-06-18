7716fc18ea080e5a89b5a4e21ea7bc314f6c4a31
rolix202 <williamsroland147@gmail.com>
2024-06-18 23:29:34 +0100

    added all the neccessary view pages

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,6 @@
 import Author from "../models/author.js";
 import asyncHandler from "express-async-handler"
+import Book from "../models/book.js";
 
 // Display list of all Authors
 export const author_list = asyncHandler(async(req, res, next) => {
@@ -10,7 +11,22 @@ export const author_list = asyncHandler(async(req, res, next) => {
 
 // Display detail page for a specific Author
 export const author_detail = asyncHandler(async(req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`)
+    const [author, allBooksByAuthor] = await Promise.all([
+        Author.findById(req.params.id).exec(),
+        Book.find({author: req.params.id}, "title summary").exec()
+    ])
+
+    if (author === null){
+        const err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("author_detail", {
+        title: "Author Detail",
+        author: author,
+        author_books: allBooksByAuthor
+    })
 })
 
 // Display Author create form on GET
